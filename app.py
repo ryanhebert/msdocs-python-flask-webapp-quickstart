e9a59d748e9095143e13685f86b98b077ca1dc06
Ryan <ryan.hebert@gmail.com>
2022-07-20 11:24:55 -0400

    Update app.py
    reset back to default

--- app.py
+++ app.py
@@ -6,30 +6,24 @@ app = Flask(__name__)
 @app.route('/')
 def index():
    print('Request for index page received')
-   
-   args = request.args.to_dict()
-   
-   return render_template('index.html', args)
+   return render_template('index.html')
 
-#@app.route('/favicon.ico')
-#def favicon():
-#    return send_from_directory(os.path.join(app.root_path, 'static'),
-#                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-#@app.route('/hello', methods=['POST'])
-#def hello():
-#   name = request.form.get('name')
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
 
-#   if name:
-#       print('Request for hello page received with name=%s' % name)
-#       return render_template('hello.html', name = name)
-#   else:
-#       print('Request for hello page received with no name or blank name -- redirecting')
-#       return redirect(url_for('index'))
+   if name:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('hello.html', name = name)
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
 
-#@app.route('/auth/login/aad/callback')
-#def test():
-#   return render_template('hello.html', name = 'test')
 
 if __name__ == '__main__':
    app.run()
